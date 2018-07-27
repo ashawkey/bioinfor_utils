39e7633864b4a22f59ba9571b426561ed7737366
hawkey <hawkey1999@126.com>
2018-07-27 17:04:15 +0800

    ggtrapezoid_ver1.0

--- hiC/ggtrapezoid.R
+++ hiC/ggtrapezoid.R
@@ -1,20 +1,74 @@
-library(ggplot2)
-library(data.table)
+# some utils for coord change ---
 
-ggtrapezoid<-function(mat, start, end, dist, flip=F, type="full"){
-    #flip the matrix, considering the mat is in the format of the previously
-    #viewed heatmap. If my consideration is somewhat wrong, just comment it
-    #and try again.
-    if(flip) mat=mat[c(nrow(mat):1),]
+# chrpos(string obj): chr:st,ed
+# extension: chr:st,chr:ed
+id2chrpos<-function(id, bed){
+    colnames(bed)=c("chr","start","end","idx")
+    return(str_c(bed[idx==id,chr],":",bed[idx==id,start],",",bed[idx==id,end]))
+}
+
+region2id<-function(region, bed){
+    colnames(bed)=c("chr","start","end","idx")
+    interval=bed[1,end]-bed[1,start]
+    Chr=str_replace(region,"(.*):(.*),(.*)","\\1")
+    Start=str_replace(region,"(.*):(.*),(.*)","\\2")
+    End=str_replace(region,"(.*):(.*),(.*)","\\3")
+    
+    if(Start==Chr & End==Chr){
+        Chr=str_sub(Chr,1,4)
+        Start=bed[chr==Chr]$start[1]
+        End=bed[chr==Chr]$end[length(bed[chr==Chr]$end)]
+        id_start=bed[chr==Chr]$idx[1]
+        id_end=bed[chr==Chr]$idx[length(bed[chr==Chr]$idx)]
+    } else {
+        Start=as.integer(Start)
+        End=as.integer(End)
+        tmp = bed[chr==Chr&start<=End&end>=Start]$idx
+        id_start=tmp[1]
+        id_end=tmp[length(tmp)]
+    }
+    return(c(id_start,id_end,Chr,Start,End))
+}
+
+# region is a chrpos string
+ggtrapezoid2<-function(mat,
+                       bed,
+                       region="all",
+                       dist=-1,
+                       type="full",
+                       color="red",
+                       by=50,
+                       transposed=F,
+                       collision_mininal_distance=10
+                       ){
+    # input transform
+    colnames(bed) = c("chrx","startx","endx","idx")
+    if(region=="all"){
+        cat("[INFO] Region is set to whole genome as default.\n")
+        Chr="Genome"
+        start = bed$idx[1]
+        end = bed$idx[length(bed$idx)]
+    }else{
+        tmp = region2id(region,bed)
+        start = as.integer(tmp[1])
+        end = as.integer(tmp[2])
+        Chr = tmp[3]
+        Start = as.integer(tmp[4])
+        End = as.integer(tmp[5])
+    }
     # constants
+    binSize=bed[1,endx]-bed[1,startx]
+    by=by*binSize
     diag_len = end - start + 1
     total_num = diag_len*diag_len
+    if(dist == -1 | dist > diag_len) {
+        dist=diag_len
+        cat("[INFO] Dist is set to max value ",diag_len," as default.\n")
+    }
     # error input 
-    if(nrow(mat) != ncol(mat) |
-       diag_len > nrow(mat) |
-       dist > diag_len )
+    if(diag_len > nrow(bed))
     {
-        return("Input data error!")
+        return("[ERROR] Region is larger than input data range.\n")
     }
     # calculate all centers of diamond polygons to be plotted
     # use vectorized operation to speed up
@@ -37,25 +91,59 @@ ggtrapezoid<-function(mat, start, end, dist, flip=F, type="full"){
     diamonds_vertices=rbind(diamonds_up,diamonds_right,diamonds_down,diamonds_left)
 
     # crop the region interested
-    mat2=mat[start:end,start:end]
-    # for debug, print what we plot (t(mat2) clockwise 45 degree --> plot)
-    # print(t(mat2))
-    # flatten the matrix for plotting
-    values=data.table(id=1:total_num,val=as.vector(as.matrix(t(mat2))))
+    colnames(mat)=c("i","j","val")
+    mmat=mat[i<=end&i>=start&j<=end&j>=start]
+    mmat[,i:=i-start+1]
+    mmat[,j:=j-start+1]
+    if(!transposed){
+        mmat[,id:=(j-1)*diag_len+i]
+    } else mmat[,id:=(i-1)*diag_len+j]
+    
+    mmat[diamonds_vertices,on="id"][,c(4:6,3)]->datapoly
+    datapoly[is.na(val)]$val=0
+
+    # log transformation
+    datapoly[,val:=log10(val+1)]
     
     # merge to create plot data
-    values[diamonds_vertices,on=("id")]->datapoly
     datapoly=datapoly[abs(y)<=dist]
     if(type=="up") datapoly=datapoly[y>=0]
     else if(type=="bottum") datapoly=datapoly[y<=0]
     
     # ggplot2
+    xmax=2*diag_len-1
+    tmp=bed[startx%%by==0 & idx>=start & idx<=end]
+    # transformed max id of current plot
+    idmax=diag_len-1+tmp$idx[1]
+    tmp=rbind(tmp,bed[idx==idmax])
+    # modification to avoid ugly overlapping labels
+    tmp$nxt=tmp$idx[c(2:length(tmp$idx),1)]
+    tmp[,dis:=nxt-idx]
+    tmp=tmp[dis<0 | dis>collision_mininal_distance]
+    # transformation from idx to real x coordinates.
+    myBreaks=2*(tmp$idx-tmp$idx[1])
+    myLabels=round(tmp$start/(binSize))
+
     p=ggplot(datapoly,aes(x,y))+
         geom_polygon(aes(fill=val,group=id))+
-        xlim(0,2*diag_len-1)+
+        scale_x_continuous(position="top",
+                           limits=c(0,xmax),
+                           expand=c(0,0),
+                           breaks=myBreaks,
+                           labels=myLabels
+                           )+
+        
+        xlab(Chr)+
         ylim(min(datapoly$y),max(datapoly$y))+
-        scale_fill_gradientn(colours=c("white","red"))+
-        coord_fixed(ratio=1)
-
+        scale_fill_gradientn(colours=c("white",color))+
+        coord_fixed(ratio=1)+
+        theme(panel.grid=element_blank(),
+              panel.background=element_blank(),
+              axis.text.y=element_blank(),
+              axis.title.y=element_blank(),
+              axis.ticks.y=element_blank(),
+              axis.line.x = element_line(size=0.7,lineend="round"),
+              axis.ticks.length=unit(.25,'cm')
+              )
     return(p)
 }
